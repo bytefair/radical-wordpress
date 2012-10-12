6b432d86a8b70e48e755915c39e71030526b7acd
Paul Graham <paul@bytefair.com>
2012-10-11 22:07:30 -0500

    redefined jquery

--- index.php
+++ index.php
@@ -6,7 +6,14 @@
             wp_title( '-', true, 'right' );
             bloginfo( 'name' );
         ?></title>
-        <?php wp_enqueue_script( 'jquery' ); ?>
+        <?php   wp_deregister_script( 'jquery' );
+                wp_register_script(
+                    'jquery',
+                    get_template_directory_uri().'/js/jquery-1.8.2.js',
+                    array(),
+                    '1.8.2',
+                    true );
+                wp_enqueue_script( 'jquery' ); ?>
         <link rel="stylesheet" href="<?php bloginfo('stylesheet_url') ?>">
         <?php wp_head(); ?>
     </head>
@@ -41,5 +48,6 @@
             </div><!-- #primary .site-content -->
 
         </div>
+        <?php wp_footer(); ?>
     </body>
 </html>
